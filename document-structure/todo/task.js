2467595f43a10d14fcd313512c4654f7dbabc898
Uldez89 <magdeev89@mail.ru>
2023-07-31 12:20:41 +0300

    Простой список дел

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,23 +1,23 @@
 const tasksInput = document.querySelector('.tasks__input');
 const tasksControl = document.querySelector('.tasks__control');
-let task = document.createElement('div');
-task.classList.add('task');
-let div = document.createElement('div');
-let taskTitle = task.appendChild(div);
-taskTitle.classList.add('task__title');
-let a = document.createElement('a');
-let taskRemove = task.appendChild(a);
-taskRemove.setAttribute('href', '#');
-taskRemove.classList.add('task__remove');
-taskRemove.innerHTML = '&times;';
 let tasksList = document.querySelector('.tasks__list');
-tasksControl.addEventListener('click', (e) => { 
+tasksControl.addEventListener('click', (e) => {
     e.preventDefault();
 })
 tasksInput.addEventListener('keyup',(e) => {
     if (e.code === 'Enter' && tasksInput.value.trim()) {
-            
+        tasksList.insertAdjacentHTML('afterBegin', '<div class="task"><div class="task__title"></div><a href="#" class="task__remove">&times;</a></div>');
+        let taskTitle = document.querySelector('.task__title');
         taskTitle.textContent = tasksInput.value;
-        tasksList.insertBefore(task,tasksList.firstElementChild);
-}
+        let taskRemoveArr = document.querySelectorAll('.task__remove');
+        taskRemoveArr.forEach((i) => {
+            i.addEventListener('click', (e) => {
+                e.preventDefault();
+                let deleted = i.closest('.task');
+                deleted.remove();
+            })
+        })
+    }
 })
+
+
