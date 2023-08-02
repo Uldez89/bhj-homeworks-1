8f53849769d14992e1071f2e3c4eb2906ba0681e
Uldez89 <magdeev89@mail.ru>
2023-08-02 17:24:12 +0300

    toolTip дораб

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,11 +1,22 @@
 const hasTooltip = document.querySelectorAll('.has-tooltip');
 let tooltipActive = document.createElement('div');
-tooltipActive.classList.add('tooltip');
 hasTooltip.forEach((item) => {
     item.addEventListener('click', (e) => {
         e.preventDefault();
+        tooltipActive.classList.add('tooltip');
         tooltipActive.classList.toggle('tooltip_active');
         tooltipActive.textContent = item.getAttribute('title');
         item.appendChild(tooltipActive);
+        let position = item.getBoundingClientRect();
+        let left = position.left + (item.offsetWidth - tooltipActive.offsetWidth) / 2;
+        if (left < 0) {
+            left = 0;
+        }
+        let top = position.top - tooltipActive.offsetHeight - 5;
+        if (top < 0) {
+            top = position.top + tooltipActive.offsetHeight + 5;
+        }
+        tooltipActive.style.left = left + 'px';
+        tooltipActive.style.top = top + 'px';
     })
 })
