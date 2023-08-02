7abb5bc454ebdc11bdf5496bc096a94f28194686
Uldez89 <magdeev89@mail.ru>
2023-08-02 17:34:04 +0300

    toвщ дораб

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,11 +1,9 @@
 const tasksInput = document.querySelector('.tasks__input');
 const tasksControl = document.querySelector('.tasks__control');
 let tasksList = document.querySelector('.tasks__list');
-tasksControl.addEventListener('click', (e) => {
+tasksControl.addEventListener('submit', (e) => {
     e.preventDefault();
-})
-tasksInput.addEventListener('keyup',(e) => {
-    if (e.code === 'Enter' && tasksInput.value.trim()) {
+    if ( tasksInput.value.trim()) {
         tasksList.insertAdjacentHTML('afterBegin', '<div class="task"><div class="task__title"></div><a href="#" class="task__remove">&times;</a></div>');
         let taskTitle = document.querySelector('.task__title');
         taskTitle.textContent = tasksInput.value;
