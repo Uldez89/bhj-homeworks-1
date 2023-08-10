a6fc13f75739e6cec22406ceaaf5ad1168785990
Uldez89 <magdeev89@mail.ru>
2023-08-10 10:06:12 +0300

    курс валют

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,5 +1,5 @@
 const pollTitle = document.querySelector('.poll__title');
-const pollAnswersActive = document.querySelector('.poll__answers_active');
+const pollAnswersActive = document.querySelector('.poll__answers');
 const xhr = new XMLHttpRequest();
 xhr.addEventListener('load', () => {
     if (xhr.readyState == xhr.DONE) {
@@ -7,7 +7,7 @@ xhr.addEventListener('load', () => {
         pollTitle.textContent = responseObj.data.title;
         for (let j = 0; j < responseObj.data.answers.length; j++) {
             let buttonAnswer = responseObj.data.answers[j];
-            pollAnswersActive.innerHTML += '<button class="poll__answer">' + buttonAnswer + '</button>';
+            pollAnswersActive.innerHTML += '<button class="poll__answer">' + buttonAnswer + '</button> ';
         } 
         const pollAnswer = document.querySelectorAll('.poll__answer');
         pollAnswer.forEach((i) => {
