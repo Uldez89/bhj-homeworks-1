14c6af70be0b8fa3057aa533eb81568a8bf9d5e9
Uldez89 <magdeev89@mail.ru>
2023-08-10 19:24:47 +0300

    progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -3,14 +3,12 @@ form.addEventListener('submit', (e) => {
     const formData = new FormData(document.forms.form);
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
-    xhr.addEventListener('readystatechange', () => {
-        if (xhr.readyState !== 4) {
-            let progress = document.getElementById('progress');
-            setInterval(() => {
-                progress.value += 0.1;
-            }, 100) 
-        }
-    })
+    xhr.upload.onprogress = function () {
+        let progress = document.getElementById('progress');
+        setInterval(() => {
+            progress.value += 0.1;
+        }, 500)
+    }
     xhr.send(formData);
     e.preventDefault();
 })
